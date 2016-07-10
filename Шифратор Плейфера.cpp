92ed05ce8edf31928b8985e780f3b4e8122ec4ae
FIexagon <pyrinov.leha@gmail.com>
2016-07-10 14:19:38 +0500

    Update Шифратор Плейфера.cpp

--- Шифратор Плейфера.cpp
+++ Шифратор Плейфера.cpp
@@ -17,9 +17,9 @@ int main() {
 	FILE *fsr;
 	FILE *fsr1;
 	FILE *code;
-	int i, j, k, l, m, n, x, y, a, pryam, b;
+	int i, j, k, l, m, n, x, y, pryam, b;
 	char P;
-	char alph[73] = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,!?-";
+	char alph[73] = "Р°Р±РІРіРґРµС‘Р¶Р·РёР№РєР»РјРЅРѕРїСЂСЃС‚СѓС„С…С†С‡С€С‰СЉС‹СЊСЌСЋСЏРђР‘Р’Р“Р”Р•РЃР–Р—РР™РљР›РњРќРћРџР РЎРўРЈР¤РҐР¦Р§РЁР©РЄР«Р¬Р­Р®РЇ .,!?-";
 	fsr = fopen("list.bin", "rt");
 	code = fopen("code.txt", "rt");
 	fsr1 = fopen("output.bin", "wt");
@@ -28,7 +28,7 @@ int main() {
 		InsertToHead(P, &phrase);
 		i++;
 	};
-	list = (char *)malloc(2 * i + 1);		//выделение нужного объёма памяти		
+	list = (char *)malloc(2 * i + 1);	
 	last = (char *)malloc(2 * i + 1);
 	WriteList(phrase, list, i);
 	list[i] = '\0';
@@ -37,14 +37,13 @@ int main() {
 		InsertToHead(P, &phrase);
 		i++;
 	};
-	word = (char *)malloc(i + 1);		//выделение нужного объёма памяти		
+	word = (char *)malloc(i + 1);	
 	word1 = (char *)malloc(i + 1);
 	WriteList(phrase, word, i);
 	word[i] = '\0';
-	strcat(word, alph);					//составление таблицы шифрования
+	strcat(word, alph);			
 	k = 0;
 	l = 0;
-	//удаление повторяющихся символов из таблицы шифрования
 	for (i = 0; i<strlen(word); i++)
 	{
 		k = 0;
@@ -60,11 +59,6 @@ int main() {
 		};
 	};
 	word1[l] = '\0';
-	for (a = 0; a < 73; a++) {
-		printf("%c ", alph[a]);
-		if (a % 8 == 7) { printf("\n"); }
-	};
-	//разбиение биграммов из одинаковых символов
 	for (i = 0; i < strlen(list); i = i + 2) {
 		if (list[i] == list[i + 1]) {
 			m = strlen(list);
@@ -79,19 +73,15 @@ int main() {
 		list[b] = '-';
 		list[b + 1] = '\0';
 	};
-	puts(list);
-	//шифрование
 	for (i = 0; i < strlen(list); i = i + 2) {
 		x = -1;
 		y = -1;
 		bigr(alph, list[i], list[i + 1], &x, &y);
-		printf("%3i %3i %c%c     ", x, y, list[i], list[i + 1]);
 		if (int(x / 8) == int(y / 8)) {
 			if (int((x + 1) / 8) == int(x / 8)) { x++; }
 			else { x = x - 6; };
 			if (int((y + 1) / 8) == int(y / 8)) { y++; }
 			else { y = y - 6; }
-			printf("1");
 		}
 		else {
 			if (int(x % 8) == int(y % 8)) {
@@ -103,42 +93,37 @@ int main() {
 				if (y > 72) {
 					y = y - 72;
 				};
-				printf("2");
 			}
 			else {
 				pryam = (int(x % 8) - int(y % 8));
 				x = x - pryam;
 				y = y + pryam;
-				printf("3");
 			};
 		};
 			last[i] = alph[x];
 			last[i + 1] = alph[y];
-		printf("     %3i %3i %c%c\n", x, y, alph[x], alph[y]);
 	};
 		last[strlen(list)] = '\0';
-	printf(last);
-	fprintf(fsr1, "%s", list);
+	fprintf(fsr1, "%s", last);
 	fclose(fsr);
 	fclose(fsr1);
 	fclose(code);
-	system("pause");
 }
-void InsertToHead(char AItem, PNode * Head) {		//чтение из файла в динамические массив
+void InsertToHead(char AItem, PNode * Head) {
 	PNode Q;
 	Q = (PNode)malloc(sizeof(TNode));
 	Q->Next = *Head;
 	Q->Sym = AItem;
 	*Head = Q;
 }
-void WriteList(PNode P, char *f, int i) { //запись в статический массив
+void WriteList(PNode P, char *f, int i) {
 	while (P != NULL) {
 		i--;
 		f[i] = P->Sym;
 		P = P->Next;
 	}
 }
-void bigr(char alph[73], char a, char b, int *k, int *j) { //получение номеров элементов биграмма в таблице
+void bigr(char alph[73], char a, char b, int *k, int *j) {
 	int i;
 	for (i = 0; i < 73; i++) {
 		if (alph[i] == a) {
