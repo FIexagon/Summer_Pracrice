1b8cad5e6698e770c6326a4f683d94e66aa16b7b
FIexagon <pyrinov.leha@gmail.com>
2016-07-13 22:02:03 +0500

    Add files via upload

---
+++ Vizenerdesh.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"	
+#include <math.h>		
+#include <string.h>		
+#include <fstream>
+struct TNode {
+	char Sym;
+	TNode *Next;
+};
+typedef TNode * PNode;
+void InsertToHead(char AItem, PNode * Head);
+void WriteList(PNode P, char *f, int i);
+int main() {
+	setlocale(LC_ALL, ".1251");
+	PNode phrase = NULL;
+	char *list;
+	FILE *fsr;
+	FILE *fsr1;
+	int i;
+	char P;
+	fsr = fopen("list.txt", "r");
+	fsr1 = fopen("output.txt", "w");
+	i = 0;
+	while (fread(&P, sizeof(char), 1, fsr)) {
+		InsertToHead(P, &phrase);
+		i++;
+	};
+	list = (char *)malloc(i + 1);
+	WriteList(phrase, list, i);
+	list[i] = '\0';
+	fprintf(fsr1, "%s", list);
+	fclose(fsr);
+	fclose(fsr1);
+}
+void InsertToHead(char AItem, PNode * Head) {
+	PNode Q;
+	Q = (PNode)malloc(sizeof(TNode));
+	Q->Next = *Head;
+	Q->Sym = AItem;
+	*Head = Q;
+}
+void WriteList(PNode P, char *f, int i) {
+	int b;
+	while (P != NULL) {
+		i--;
+		f[i] = P->Sym;
+		if (((int)(f[i]) >= -64) && ((int)(f[i]) <= -33)) {
+			b = (int)(f[i]) - i;
+			while (b < -64) {
+				b = b + 32;
+			}
+			f[i] = (char)(b);
+		}
+		else {
+			if (((int)(f[i]) >= -32) && ((int)(f[i]) <= -1)) {
+				b = (int)(f[i]) - i;
+				while (b < -32) {
+					b = b + 32;
+				}
+				f[i] = (char)(b);
+			};
+		};
+		P = P->Next;
+	}
+}
